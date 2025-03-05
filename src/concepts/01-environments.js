263a499bd24b45bc25fcb64715a661278f3e98b7
Manuel Rivas <rexdaro@gmail.com>
2025-03-05 08:47:15 -0300

    first commit

---
+++ src/concepts/01-environments.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {HTMLDivElement} element
+ */
+export const environmentsComponent = (element) => {
+
+    console.log(import.meta.env)
+
+    const html = `
+        variables:
+    `;
+    element.innerHTML = html;
+
+}
